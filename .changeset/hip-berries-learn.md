b92e05ec1dd39bdf0481f75ff21b27a0fada0460
qiuxchao <2915241235@qq.com>
2023-05-27 23:26:57 +0800

    🧩 update: eslint-config

---
+++ .changeset/hip-berries-learn.md
@@ -0,0 +1,5 @@
+---
+'@qiuxc/eslint-config': patch
+---
+
+关闭 react/jsx-no-leaked-render（已经是布尔值的也会被转换，没有必要），关闭 complexity
--- packages/eslint-config/test/react/jsx-no-leaked-render/.eslintrc.js
+++ packages/eslint-config/test/react/jsx-no-leaked-render/.eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
 	rules: {
 		/**
 		 * 使用 && 渲染组件时，禁止条件是 0 '' 或 NaN，强制转为布尔值
+		 * @reason 已经是布尔值的也会被转换，没有必要
 		 */
-		'react/jsx-no-leaked-render': ['error', { validStrategies: ['coerce'] }],
+		'react/jsx-no-leaked-render': 'off',
 	},
 };
