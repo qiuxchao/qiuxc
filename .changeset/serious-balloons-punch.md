a482eeefc17e3c08d646afaff0ca812101a0cfd5
qiuxchao <2915241235@qq.com>
2023-10-30 17:12:04 +0800

    🧩 update: taro-icon

---
+++ .changeset/serious-balloons-punch.md
@@ -0,0 +1,5 @@
+---
+'@qiuxc/taro-iconfont': major
+---
+
+first publish
--- packages/taro-iconfont/build.config.ts
+++ packages/taro-iconfont/build.config.ts
@@ -1,18 +1,15 @@
-import { copy } from "fs-extra";
-import path from "path";
-import { defineBuildConfig } from "unbuild";
+import { copy } from 'fs-extra';
+import path from 'path';
+import { defineBuildConfig } from 'unbuild';
 
 export default defineBuildConfig({
   clean: true,
   hooks: {
-    "build:done": async (context) => {
+    'build:done': async context => {
+      copy(path.resolve(__dirname, './src/templates'), path.resolve(__dirname, './dist/templates'));
       copy(
-        path.resolve(__dirname, "src/templates"),
-        path.resolve(__dirname, "dist/templates")
-      );
-      copy(
-        path.resolve(__dirname, "src/iconfont.json"),
-        path.resolve(__dirname, "dist/iconfont.json")
+        path.resolve(__dirname, './src/iconfont.json'),
+        path.resolve(__dirname, './dist/iconfont.json'),
       );
     },
   },
